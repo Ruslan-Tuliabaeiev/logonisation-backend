c7a4a1d9d48135fa740139f6827f967bb5fb7a38
Ruslan-Tuliabaeiev <ruhus9111@gmail.com>
2024-08-30 08:54:04 +0300

    Fixing files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {AuthEntity} from "./auth/entity/auth.entity";
-import {AuthModule} from "./auth/auth.module";
-import {Articlemodule} from "./article/articlemodule";
-import {ArticleEntity} from "./article/entity/article.entity";
-
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthEntity } from './auth/entity/auth.entity';
+import { AuthModule } from './auth/auth.module';
+import { Articlemodule } from './article/articlemodule';
+import { ArticleEntity } from './article/entity/article.entity';
 
 @Module({
   imports: [
@@ -19,8 +17,8 @@ import {ArticleEntity} from "./article/entity/article.entity";
       entities: [AuthEntity, ArticleEntity],
       synchronize: true,
     }),
-      AuthModule,
-      Articlemodule,
+    AuthModule,
+    Articlemodule,
   ],
 })
 export class AppModule {}
--- src/auth/entity/auth.entity.ts
+++ src/auth/entity/auth.entity.ts
@@ -1,31 +1,30 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
-import {IsDefined, IsNotEmpty, IsString} from "class-validator";
-import {ApiProperty} from "@nestjs/swagger";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDefined, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('auth')
 export class AuthEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @ApiProperty()
-    @Column()
-    @IsNotEmpty()
-    @IsString()
-    @IsDefined()
-    name: string;
+  @ApiProperty()
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  @IsDefined()
+  name: string;
 
-    @ApiProperty()
-    @Column({unique: true})
-    @IsNotEmpty()
-    @IsString()
-    @IsString()
-    email: string;
+  @ApiProperty()
+  @Column({ unique: true })
+  @IsNotEmpty()
+  @IsString()
+  @IsString()
+  email: string;
 
-    @ApiProperty()
-    @Column()
-    @IsNotEmpty()
-    @IsDefined()
-    @IsString()
-    password: string;
-
+  @ApiProperty()
+  @Column()
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
+  password: string;
 }
